da27df59430d5c78fcc5e262fe7781c403e0f9ee
melendezcabreram <melendezcabreram@gmail.com>
2023-03-22 19:03:28 +0100

    primer commit creasion

---
+++ database.js
@@ -0,0 +1,17 @@
+
+const mysql = require('mysql2')
+
+
+let connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "poiu",
+    database: "petfinder"
+});
+
+connection.connect(function (err, res) {
+    if (err) console.log(err);
+    else console.log("MySQL Database connected!");
+});
+
+module.exports = connection;
