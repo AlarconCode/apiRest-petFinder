dba5553ac166fd59f876be83e01312d2eba6aae1
melendezcabreram <melendezcabreram@gmail.com>
2023-03-23 12:54:31 +0100

    get done

---
+++ controller/post-controller.js
@@ -0,0 +1,19 @@
+const connection = require("../database")
+
+function getPosts(request, response){
+    let sql;
+
+    if(request.query.id_user){
+        sql= `SELECT * FROM posts WHERE id_user=${request.query.id_user}`
+    }else{
+        `SELECT * FROM posts`
+    }
+
+    connection.query(sql, function (err, result) {
+        if (err)
+            console.log(err);
+        else {
+            response.send(result)
+        }
+    })
+}
