550b3107d1b0b64078703a8a784c8ff438ef6f05
AlarconCode <julian.alarcon81@gmail.com>
2023-03-23 17:15:21 +0100

    cambio let por const en connection

--- database.js
+++ database.js
@@ -2,7 +2,7 @@
 const mysql = require('mysql2')
 
 
-let connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "poiu",
