3e64d575f08c3e16a4bf8b967db782d9ad3d82e1
AlarconCode <julian.alarcon81@gmail.com>
2023-03-23 20:04:10 +0100

    creada funcion putUser y routers

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const errorHandling = require("./error/errorHandling");
+const userRouter = require('./routers/user-router');
+
 
 
 
@@ -10,13 +12,14 @@ app.set('port', process.env.PORT || 3000)
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 app.use(cors())
-app.use(function (req, res, next) {
-    res.status(404).json({
-        error: true,
-        codigo: 404,
-        message: "Endpoint doesn't found"
-    })
-})
+// app.use(function (req, res, next) {
+//     res.status(404).json({
+//         error: true,
+//         codigo: 404,
+//         message: "Endpoint doesn't found"
+//     })
+// })
 app.use(errorHandling);
+app.use(userRouter)
 
 module.exports = app;
--- controller/user-controller.js
+++ controller/user-controller.js
@@ -5,11 +5,11 @@ const postUser = (req, res) => {
 
     let response = {error: true, code:500, result: []}
 
-    const {userName, password, email, userLocation, phone} = req.body
+    const {user_name, password, email, user_location, phone, user_image} = req.body
 
-    let params = [userName, password, email, userLocation, phone]
+    let params = [user_name, password, email, user_location, phone, user_image]
 
-    let sql = `INSERT INTO Users (userName, password, email, userLocation, phone) VALUES (?, ?, ?, ?, ?);`
+    let sql = `INSERT INTO Users (user_name, password, email, user_location, phone, user_image) VALUES (?, ?, ?, ?, ?, ?);`
 
     connection.query(sql, params, (err, result) => {
 
@@ -27,3 +27,32 @@ const postUser = (req, res) => {
     })
 
 }
+
+const putUser = (req, res) => {
+
+    let response = {error: true, code:500, result: []}
+
+    const {id_user, user_name, password, email, user_location, phone, user_image} = req.body
+
+    let params = [user_name, password, email, user_location, phone, user_image, id_user]
+
+    let sql = `UPDATE Users SET user_name = COALESCE(?, user_name), password = COALESCE(?, password), email = COALESCE(?, email), user_location = COALESCE(?, user_location), phone = COALESCE(?, phone), user_image = COALESCE(?, user_image)  WHERE id_user = ?`
+
+    connection.query(sql, params, (err, result) => {
+
+        if (err) {
+            console.log(err);
+            res.send(response)
+        } else {
+            console.log(result);
+            response.error = false
+            response.code = 200,
+            response.result = result
+            res.send(response)
+        }
+
+    })
+
+}
+
+module.exports = {postUser, putUser}
