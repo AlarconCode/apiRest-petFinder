4c29f3b9c3fcb393030ea17a136e1155fb839d09
AlarconCode <julian.alarcon81@gmail.com>
2023-03-24 13:20:31 +0100

    añadido funcion postLogin

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -55,4 +55,36 @@ const putUser = (req, res) => {
 
 }
 
-module.exports = {postUser, putUser}
+const postLogin = (req, res) => {
+
+    let response = {error: true, code:500, result: []}
+
+    const {email, password} = req.body
+
+    let params = [email, password]
+
+    let sql = `SELECT id_user, user_name, password, email, user_location, phone, user_image FROM Users WHERE email = ? AND password = ?`
+
+    connection.query(sql, params, (err, result) => {
+
+        if(err) {
+            console.log(err);
+        } else {
+            console.log(result);
+            if(result.length == 0) {
+                response.error = true
+                response.code = 404,
+                response.result = 'Los datos no existen'
+            } else {
+                response.error = false
+                response.code = 200,
+                response.result = result
+            }
+            res.send(response)
+        }
+
+    })
+
+}
+
+module.exports = {postUser, putUser, postLogin}
--- routers/user-router.js
+++ routers/user-router.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const { postUser, putUser } = require('../controller/user-controller')
+const { postUser, putUser, postLogin } = require('../controller/user-controller')
 const userRouter = express.Router()
 
 userRouter.post('/users', postUser)
 userRouter.put('/users', putUser)
+userRouter.post('/login', postLogin)
 
 module.exports = userRouter
