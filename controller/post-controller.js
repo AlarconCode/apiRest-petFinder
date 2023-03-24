a8443a71dd38cf7dd8ca68a71d1145a25b1178f1
melendezcabreram <melendezcabreram@gmail.com>
2023-03-24 15:22:34 +0100

    commit para ir

--- controller/post-controller.js
+++ controller/post-controller.js
@@ -58,6 +58,33 @@ function putPublication(request, response) {
     })
 }
 
+function getFoundPublication() {
+
+    let respuesta = { error: true, code: 500, result: [] };
+   
+    let id_post = request.query.id_post;
+     // const { id_post, found } = request.body
+    const {found} = request.body;
+    let params = [found, id_post]
+
+    let sql = `SELECT found FROM Posts WHERE id_post=?`
+
+    connection.query(sql, params, function (err, result) {
+        if (err) {
+            console.log(err);
+            response.send(respuesta)
+        }
+        else {
+            console.log(result)
+            respuesta.error = false;
+            respuesta.code = 200;
+            respuesta.result = result;
+            response.send(respuesta)
+        }
+    })
+}
+
+
 
 function putFoundPublication(request, response) {
 
@@ -108,5 +135,5 @@ const postPublication = (req, res) => {
     })
 
 }
-module.exports = { getPublications, putPublication, putFoundPublication, postPublication }
+module.exports = { getPublications, putPublication, putFoundPublication, postPublication, getFoundPublication }
 
--- routers/post-router.js
+++ routers/post-router.js
@@ -1,12 +1,13 @@
 const express = require('express')
-const { getPublications, putPublication, putFoundPublication, postPublication, deletePublication } = require('../controller/post-controller')
+const { getPublications, putPublication, putFoundPublication, getFoundPublication, postPublication, deletePublication } = require('../controller/post-controller')
 const postRouter = express.Router()
 
 postRouter.get("/posts", getPublications)
 postRouter.put("/posts", putPublication)
 postRouter.put("/posts", putFoundPublication)
+postRouter.get("/posts", getFoundPublication)
 postRouter.post('/posts', postPublication)
-postRouter.delete('/posts', deletePublication)
+// postRouter.delete('/posts', deletePublication)
 
 
 module.exports = postRouter
