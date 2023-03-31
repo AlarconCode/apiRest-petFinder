bfc33ed1c1c5b8254ae911173c6c19d44bdf75b0
melendezcabreram <melendezcabreram@gmail.com>
2023-03-31 21:09:53 +0200

    seguir mañana

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const cors = require('cors')
 const errorHandling = require("./error/errorHandling");
 const userRouter = require('./routers/user-router');
 const postRouter = require('./routers/post-router');
+//SENDEMAIL
+const nodemailer = require('nodemailer')
+const bodyParser = require('body-parser')
 
 
 // MiddleWares
 app.set('port', process.env.PORT || 3000)
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cors())
 // app.use(function (req, res, next) {
@@ -20,6 +23,18 @@ app.use(cors())
 //     })
 // })
 app.use(errorHandling);
+
+//SENDEMAIL
+app.use(bodyParser.json())
+// app.use(cors({ origin: "*" }))
+app.post("/sendemail", (request, response) => {
+    console.log("request came");
+    let user = req.body;
+    SendmailTransport(user, info => {
+        console.log(`The mail has been send and the id is ${info.messageId}`);
+        response.send(info);
+    })
+})
 
 app.use(userRouter, postRouter)
 
