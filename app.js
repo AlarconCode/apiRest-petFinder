ff286de6215a6f13ffc58559d2e99f6cdcc67971
melendezcabreram <melendezcabreram@gmail.com>
2023-04-03 19:03:14 +0200

    error en el nodemon pero no importa

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const errorHandling = require("./error/errorHandling");
 const userRouter = require('./routers/user-router');
 const postRouter = require('./routers/post-router');
 //SENDEMAIL
-const nodemailer = require('nodemailer')
+// const nodemailer = require('nodemailer')
 const bodyParser = require('body-parser')
 
 
@@ -24,32 +24,29 @@ app.use(cors())
 // })
 app.use(errorHandling);
 
-//SENDEMAIL START
-app.use(bodyParser.json())
-// app.use(cors({ origin: "*" }))
-app.post("/sendemail", (request, response) => {
-    console.log("request came");
-    let user = req.body;
-    SendmailTransport(user, info => {
-        console.log(`The mail has been send and the id is ${info.messageId}`);
-        response.send(info);
-    })
-})
+app.post('/formulario', (req, res) => {})
+app.listen(3000, () => {
+console.log('Servidor corriendo')
+});
 
-async function sendMail(user, callback){
-    //create reusable transporter object using the default SMYP transport
-    let transporter = nodemailer.createTransport({
-        host:"smtp.gmail.com",
-        port:587,
-        secure:false, // true for 465, false for other ports
-        auth:{
-            user:details.email,
-            pass: details.password
-        
-        }
-    })
-}
-//SENDEMAIL  END
+
+//CODIGO DE ANDY SANTANA
+
+// const express = require("express");
+// const cors = require("cors");
+// const bodyParser = require("body-parser");
+const configMensaje = require("./configMensaje");
+// const app = express();
+app.use(bodyParser.json());
+// app.use(cors())
+app.post("/formulario", (req, res) => {
+ configMensaje(req.body);
+ res.status(200).send();
+})
+app.listen(3000, () => {
+ console.log("Servidor corriendo")
+});
+
 
 app.use(userRouter, postRouter)
 
