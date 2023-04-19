4c420ad8bcd18fdb65f6855b748f9a5b398f684a
AlarconCode <julian.alarcon81@gmail.com>
2023-04-19 19:56:50 +0200

    error handling

--- src/app.js
+++ src/app.js
@@ -12,13 +12,13 @@ app.set('port', process.env.PORT || 3000)
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 app.use(cors())
-// app.use(function (req, res, next) {
-//     res.status(404).json({
-//         error: true,
-//         codigo: 404,
-//         message: "Endpoint doesn't found"
-//     })
-// })
+app.use(function (req, res, next) {
+    res.status(404).json({
+        error: true,
+        codigo: 404,
+        message: "Endpoint doesn't found"
+    })
+})
 app.use(errorHandling);
 app.get('/', (req, res) => {
     res.send('API DESPLEGADA!')
