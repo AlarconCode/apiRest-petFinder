d4f43cda8ff8375e5494b03f033d533d93d7f246
AlarconCode <julian.alarcon81@gmail.com>
2024-06-10 19:21:14 +0200

    prueba de cors sin origins

--- src/app.js
+++ src/app.js
@@ -7,23 +7,24 @@ if (process.env.NODE_ENV !== 'production') require('dotenv').config()
     const postRouter = require('./routers/post-router');
     const emailRouter = require('./routers/email-router');
     // Origins
-    const corsWhiteList = ["https://pet-finder-livid.vercel.app", "http://localhost:4200"]
-    const corsConfig = {
-      credentials: true,
-      optionsSuccessStatus: 200,
-      origin: function(origin, callback) {
-        const isOriginInWhiteList = origin && corsWhiteList.includes(origin);
-        callback(null, isOriginInWhiteList);
-      }
-    }
+    // const corsWhiteList = ["https://pet-finder-livid.vercel.app", "http://localhost:4200"]
+    // const corsConfig = {
+    //   credentials: true,
+    //   optionsSuccessStatus: 200,
+    //   origin: function(origin, callback) {
+    //     const isOriginInWhiteList = origin && corsWhiteList.includes(origin);
+    //     callback(null, isOriginInWhiteList);
+    //   }
+    // }
     
     
     // MiddleWares
     app.set('port', process.env.PORT || 3000)
     app.use(express.urlencoded({extended:false}))
     app.use(express.json());
-    app.use(cors(corsConfig));
-    app.options('*', cors(corsConfig));
+    app.use(cors());
+    // app.use(cors(corsConfig));
+    // app.options('*', cors(corsConfig));
     // app.use(function (req, res, next) {
     //     res.status(404).json({
     //         error: true,
